f4fe07b18fdb4e4cab4c8c148372b4989fe0eba3
abdulsalamwasti <66717591+abdulsalamwasti@users.noreply.github.com>
2020-06-14 20:39:08 +0500

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -1,89 +1,29 @@
-//#include <iostream>
-//#include <stdlib.h>
-//using namespace std;
-//
-//void resetArray(char[7][7], int);
-//void Board(char[][7], int);
-//void player1(char[][7], char, int);
-//void player2(char[][7], char, int);
-//void gamecheck(char[][7], int, bool&);
-//
-//int main()
-//{
-//
-//	int fd;
-//	char *myfifo = "tic";
-//	//mkfifo(myfifo, 0666);
-//	char arr1[20], arr2[20];
-//
-//	char choice;
-//	bool turn = 1, back = 1;
-//	char board[7][7];
-//	resetArray(board, 7);
-//	while (back == 1)
-//	{
-//		printf("\n\t   T i c - T a c - T o e \n\n");;
-//		Board(board, 7);
-//		printf("[X] For First Player \n");
-//		printf("[O] For Second Player \n\n");
-//		if (turn == 1)
-//		{
-//			//fd = open(myfifo, O_WRONLY);
-//			printf("\tTurn of first Player\n");
-//			printf("Enter your choice [X]:");
-//			cin >> choice;
-//			arr1[0] = choice;
-//			//write(fd, arr1, 20);
-//			//close(fd);
-//			player1(board, choice, 7);
-//			turn = 0;
-//		}
-//		else
-//		{
-//			//fd = open(myfifo, O_RDONLY);
-//			//read(fd, arr2, 20);
-//			//close(fd);
-//			choice = arr2[0];
-//			player2(board, choice, 7);
-//			turn = 1;
-//		}
-//		
-//		system("cls");
-//		gamecheck(board, 7, back);
-//		system("cls");
-//		if (back == 0)
-//		{
-//			break;
-//		}
-//	}
-//	printf("\n");
-//#include <iostream>
-//#include <stdlib.h>
-//#include<stdio.h>
-//#include<unistd.h>
-//#include<sys/types.h>
-//#include<sys/stat.h>
-//#include<fcntl.h>
-//
-//	using namespace std;
-//
-//	void resetArray(char[7][7], int);
-//	void Board(char[][7], int);
-//	void player1(char[][7], char, int);
-//	void player2(char[][7], char, int);
-//	void gamecheck(char[][7], int, bool&);
-//
-//
-//	int fd;
-//	char *myfifo = "tic";
-//	char arr1[20], arr2[20];
-//
-//	int main()
-//	{
-//
-//		mkfifo(myfifo, 0666);
-//
-//		char choice;
+#include <iostream>
+#include <stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+	using namespace std;
+
+	void resetArray(char[7][7], int);
+	void Board(char[][7], int);
+	void player1(char[][7], char, int);
+	void player2(char[][7], char, int);
+	void gamecheck(char[][7], int, bool&);
+
+
+	int fd;
+	char *myfifo = "tic";
+	char arr1[20], arr2[20];
+	int main()
+	{
+
+		mkfifo(myfifo, 0666);
+
+		char choice;
 //		bool turn = 1, back = 1;
 //		char board[7][7];
 //		resetArray(board, 7);
@@ -400,77 +340,77 @@
 //}
 ////////////////
 //void gamecheck(char board[][7], int s, bool& back)
-//{
-//	char game;
-//
-//	if (board[1][0] == 'X'&&board[1][2] == 'X'&&board[1][4] == 'X' ||
-//		board[3][0] == 'X'&&board[3][2] == 'X'&&board[3][4] == 'X' ||
-//		board[5][0] == 'X'&&board[5][2] == 'X'&&board[5][4] == 'X' ||
-//		board[1][0] == 'X'&&board[3][2] == 'X'&&board[5][4] == 'X' ||
-//		board[1][4] == 'X'&&board[3][2] == 'X'&&board[5][0] == 'X' ||
-//		board[1][0] == 'X'&&board[3][0] == 'X'&&board[5][0] == 'X' ||
-//		board[1][2] == 'X'&&board[3][2] == 'X'&&board[5][2] == 'X' ||
-//		board[1][4] == 'X'&&board[3][4] == 'X'&&board[5][4] == 'X')
-//	{
-//		Board(board, 7);
-//		printf("\t  :Player 1 win:\n");
-//		printf("Do you Want to play Again ! (Y or N): ");
-//		cin >> game;
-//		if (game == 'Y' || game == 'y')
-//		{
-//			resetArray(board, 7);
-//			back = 1;
-//		}
-//		else
-//		{
-//			back = 0;
-//		}
-//
-//	}
-//
-//	if (board[1][0] == 'O'&&board[1][2] == 'O'&&board[1][4] == 'O' ||
-//		board[3][0] == 'O'&&board[3][2] == 'O'&&board[3][4] == 'O' ||
-//		board[5][0] == 'O'&&board[5][2] == 'O'&&board[5][4] == 'O' ||
-//		board[1][0] == 'O'&&board[3][2] == 'O'&&board[5][4] == 'O' ||
-//		board[1][4] == 'O'&&board[3][2] == 'O'&&board[5][0] == 'O' ||
-//		board[1][0] == 'O'&&board[3][0] == 'O'&&board[5][0] == 'O' ||
-//		board[1][2] == 'O'&&board[3][2] == 'O'&&board[5][2] == 'O' ||
-//		board[1][4] == 'O'&&board[3][4] == 'O'&&board[5][4] == 'O')
-//	{
-//		Board(board, 7);
-//		printf("\t  :Player 2 win: \n");
-//		printf("Do you Want to play Again ! (Y or N): ");
-//		cin >> game;
-//		if (game == 'Y' || game == 'y')
-//		{
-//			resetArray(board, 7);
-//			back = 1;
-//		}
-//		else
-//		{
-//			back = 0;
-//		}
-//
-//	}
-//
-//	if (board[1][0] != '1' && board[1][2] != '2' &&
-//		board[1][4] != '3' && board[3][0] != '4' &&
-//		board[3][2] != '5' && board[3][4] != '6' &&
-//		board[5][0] != '7' && board[5][2] != '8' &&
-//		board[5][4] != '9')
-//	{
-//		Board(board, 7);
-//		printf("\t  Match Draw: \n");
-//		printf("Do you Want to play Again ! (Y or N):");
-//		cin >> game;
-//		if (game == 'Y' || game == 'y')
-//		{
-//			resetArray(board, 7);
-//			back = 1;
-//		}
-//		else
-//		{
-//			back = 0;
-//		}
-//	}
-//}
+{
+	char game;
+
+	if (board[1][0] == 'X'&&board[1][2] == 'X'&&board[1][4] == 'X' ||
+		board[3][0] == 'X'&&board[3][2] == 'X'&&board[3][4] == 'X' ||
+		board[5][0] == 'X'&&board[5][2] == 'X'&&board[5][4] == 'X' ||
+		board[1][0] == 'X'&&board[3][2] == 'X'&&board[5][4] == 'X' ||
+		board[1][4] == 'X'&&board[3][2] == 'X'&&board[5][0] == 'X' ||
+		board[1][0] == 'X'&&board[3][0] == 'X'&&board[5][0] == 'X' ||
+		board[1][2] == 'X'&&board[3][2] == 'X'&&board[5][2] == 'X' ||
+		board[1][4] == 'X'&&board[3][4] == 'X'&&board[5][4] == 'X')
+	{
+		Board(board, 7);
+		printf("\t  :Player 1 win:\n");
+		printf("Do you Want to play Again ! (Y or N): ");
+		cin >> game;
+		if (game == 'Y' || game == 'y')
+		{
+			resetArray(board, 7);
+			back = 1;
+		}
+		else
+		{
+			back = 0;
+		}
+
+	}
+
+	if (board[1][0] == 'O'&&board[1][2] == 'O'&&board[1][4] == 'O' ||
+		board[3][0] == 'O'&&board[3][2] == 'O'&&board[3][4] == 'O' ||
+		board[5][0] == 'O'&&board[5][2] == 'O'&&board[5][4] == 'O' ||
+		board[1][0] == 'O'&&board[3][2] == 'O'&&board[5][4] == 'O' ||
+		board[1][4] == 'O'&&board[3][2] == 'O'&&board[5][0] == 'O' ||
+		board[1][0] == 'O'&&board[3][0] == 'O'&&board[5][0] == 'O' ||
+		board[1][2] == 'O'&&board[3][2] == 'O'&&board[5][2] == 'O' ||
+		board[1][4] == 'O'&&board[3][4] == 'O'&&board[5][4] == 'O')
+	{
+		Board(board, 7);
+		printf("\t  :Player 2 win: \n");
+		printf("Do you Want to play Again ! (Y or N): ");
+		cin >> game;
+		if (game == 'Y' || game == 'y')
+		{
+			resetArray(board, 7);
+			back = 1;
+		}
+		else
+		{
+			back = 0;
+		}
+
+	}
+
+	if (board[1][0] != '1' && board[1][2] != '2' &&
+		board[1][4] != '3' && board[3][0] != '4' &&
+		board[3][2] != '5' && board[3][4] != '6' &&
+		board[5][0] != '7' && board[5][2] != '8' &&
+		board[5][4] != '9')
+	{
+		Board(board, 7);
+		printf("\t  Match Draw: \n");
+		printf("Do you Want to play Again ! (Y or N):");
+		cin >> game;
+		if (game == 'Y' || game == 'y')
+		{
+			resetArray(board, 7);
+			back = 1;
+		}
+		else
+		{
+			back = 0;
+		}
+	}
+}
